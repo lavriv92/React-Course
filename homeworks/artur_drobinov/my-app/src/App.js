da7326bdaa907a42e3a53a170eabab2ad6f3665b
artur.drobinov <Artur.Drobinov@symphony-solutions.com>
2020-03-05 19:09:57 +0200

    save lesson 1 changes

---
+++ homeworks/artur_drobinov/my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import LessonOne from "./lesson1/lesson-one";
+
+function App() {
+
+  return (
+      <LessonOne />
+  );
+}
+
+export default App;
---
+++ homeworks/artur_drobinov/my-app/src/lesson1/comments/comments.container.js
@@ -0,0 +1,12 @@
+import React from "react";
+export default class Comments extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Привет,vsh
+            </div>
+        );
+    }
+}
+
