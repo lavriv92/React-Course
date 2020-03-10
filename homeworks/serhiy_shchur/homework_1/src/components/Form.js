1217fb2503e44e38aa40f984c92db86e0cc5cf95
Sergey Shchur <sergey.shchur@gmail.com>
2020-03-10 19:01:14 +0200

    small form fix

--- homeworks/serhiy_shchur/homework_1/src/components/Form.js
+++ homeworks/serhiy_shchur/homework_1/src/components/Form.js
@@ -36,16 +36,16 @@ export default class Form extends React.Component {
             errors.username.push('Username can\'t be empty');
         }
         if( !this.state.form.password.length ) {
-            errors.username.push('Password can\'t be empty');
+            errors.password.push('Password can\'t be empty');
         }
         if( this.state.form.username.length < 3 ) {
             errors.username.push('Username should be 3 chars at least');
         }
         if( this.state.form.password.length < 8 ) {
-            errors.username.push('Password should be 8 chars at least');
+            errors.password.push('Password should be 8 chars at least');
         }
         if( !this.state.form.password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]$/) ) {
-            errors.username.push('Password should contain at least one letter and one number');
+            errors.password.push('Password should contain at least one letter and one number');
         }
         this.setState(
             {
